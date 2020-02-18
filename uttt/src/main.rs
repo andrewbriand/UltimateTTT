3de7231dd9ae1fac693df03f2378b4a584c335b3
O. O. Man <atb8888@comcast.net>
2020-02-17 18:24:42 -0800

    Add text_io

--- uttt/src/main.rs
+++ uttt/src/main.rs
@@ -2,6 +2,7 @@ use structopt::StructOpt;
 mod board;
 pub use board::Board;
 use board::Player;
+use text_io::read;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -17,40 +18,44 @@ fn main() {
     let mut b = Board::new(2);
     b.pretty_print();
     println!("{:?}", args.x_ai_path);
-    assert!(b.make_move(6));
-    assert!(b.make_move(0));
+    //assert!(b.make_move(6));
+    //assert!(b.make_move(0));
 
-    assert!(!b.make_move(28));
-    assert!(b.make_move(9));
-    assert!(b.make_move(10));
+    //assert!(!b.make_move(28));
+    //assert!(b.make_move(9));
+    //assert!(b.make_move(10));
 
-    assert!(!b.make_move(22));
-    assert!(b.make_move(31));
-    assert!(b.make_move(20));
+    //assert!(!b.make_move(22));
+    //assert!(b.make_move(31));
+    //assert!(b.make_move(20));
 
-    assert!(!b.make_move(37));
-    assert!(b.make_move(79));
-    assert!(b.make_move(30));
+    //assert!(!b.make_move(37));
+    //assert!(b.make_move(79));
+    //assert!(b.make_move(30));
 
-    assert!(!b.make_move(36));
-    assert!(b.make_move(19));
-    assert!(b.make_move(40));
+    //assert!(!b.make_move(36));
+    //assert!(b.make_move(19));
+    //assert!(b.make_move(40));
 
-    assert!(!b.make_move(5));
-    assert!(b.make_move(39));
-    assert!(b.make_move(50));
+    //assert!(!b.make_move(5));
+    //assert!(b.make_move(39));
+    //assert!(b.make_move(50));
 
-    assert!(!b.make_move(64));
-    assert!(b.make_move(62));
-    assert!(b.make_move(60));
+    //assert!(!b.make_move(64));
+    //assert!(b.make_move(62));
+    //assert!(b.make_move(60));
 
-    assert!(!b.make_move(72));
-    assert!(b.make_move(11));
-    assert!(b.make_move(70));
+    //assert!(!b.make_move(72));
+    //assert!(b.make_move(11));
+    //assert!(b.make_move(70));
 
-    assert!(!b.make_move(77));
-    assert!(b.make_move(32));
-    assert!(b.make_move(80));
-    b.pretty_print();
+    //assert!(!b.make_move(77));
+    //assert!(b.make_move(32));
+    //assert!(b.make_move(80));
+    while true {
+        let i: usize = read!();
+        println!("{}", b.make_move(i));
+        b.pretty_print();
+    }
     println!("{:?}", b.winner);
 }
